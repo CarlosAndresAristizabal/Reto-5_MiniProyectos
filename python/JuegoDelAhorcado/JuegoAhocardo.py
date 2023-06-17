eef0be1a2bc0914744597a57bbe785b486240da7
Carlos Andres <carlosaristi22@gmail.com>
2023-06-16 22:12:54 -0500

    miniproyecto juego del ahorcado con python

---
+++ python/JuegoDelAhorcado/JuegoAhocardo.py
@@ -0,0 +1,93 @@
+# se importan librerias que se usaran
+import random
+import string
+# se importa archivo externo para los diagramas
+from diagramAhorcado import vidas_diccionario_visual
+
+# se crea variable de la palabra que el jugador está tratando de adivinar
+palabras = ["aire", "ojos", "piel", "anteojos", "joven", "viejo", "alto", "bajo", "pequeño", "gordo", "delgado", "bella", "azul", "verde", "negro", "sombrero", "guantes", "corbata", "gemelos", "paraguas", "plata", "oro", "perla", "diamante", "esmeralda", "anillo", "pulsera", "reloj", "elegante", "sencillo", "chaqueta", "traje", "camisa", "zapatos", "pelo", "maquillaje", "peine", "dedo", "hueso", "cara", "ojo", "calor", "ambulancia", "enfermera", "farmacia", "vitaminas", "pastillas", "dentista", "ciego", "correr", "caminar", "regresar", "saltar", "fin", "cerrar", "nombre", "mujer", "hombre", "soltero", "novio", "nacer", "vivir", "edad", "anciana", "trabajar", "cobrar", "azafata", "artista", "panadero", "carpintero", "cocinero", "maestro", "bombero", "juez", "modelo", "monje", "pintor", "piloto", "secretaria", "taxista", "escritor", "jefe", "aprendiz", "jubilado", "empleo", "industria", "taller", "tienda", "vacaciones", "sueldo", "impuesto", "huelga", "obedecer", "locura", "humor", "inteligencia", "orgullo", "timidez", "valiente", "aburrido", "loco", "divertido", "bueno", "feliz", "amable", "pobre", "serio", "extraño", "gritar", "llorar", "suspirar", "preocupado", "risa", "amor", "suerte", "enamorado", "ver", "apagar", "luz", "color", "lupa", "microscopio", "claro", "cantar", "silbar", "voz", "eco", "trueno", "altavoz", "radio", "auricular", "liso", "comer", "dulce", "salado", "perfume", "despertarse", "vestirse", "desayunar", "leer", "dormirse", "toalla", "cobija", "almuerzo", "sopa", "agua", "leche", "jugo", "sal", "pimienta", "vinagre", "ajo", "perejil", "menta", "canela", "mayonesa", "pan", "mantequilla", "miel", "manzana", "pera", "durazno", "cereza", "papa", "lechuga", "arroz",
+            "pollo", "pavo", "hamburguesa", "camarones", "tortilla", "espagueti", "sopa", "helado", "chocolate", "galletas", "bombones", "limpiar", "cortar", "hervir", "planchar", "aspiradora", "plancha", "horno", "abrelatas", "vajilla", "vaso", "cafetera", "azucarera", "comprar", "gastar", "vender", "barato", "caro", "gratis", "cliente", "bolsa", "precio", "recibo", "ascensor", "esquiar", "ciclismo", "golf", "pelota", "tenis", "bicicleta", "estadio", "gol", "torneo", "leer", "dibujar", "cantar", "bailar", "libro", "revista", "clavo", "cine", "pala", "cocina", "hielo", "coro", "piano", "cartas", "pesca", "radio", "noticias", "televisor", "documental", "anuncio", "aplaudir", "teatro", "circo", "mago", "disco", "portero", "propina", "regalo", "fiesta", "vela", "alfombra", "puerta", "ventana", "cortina", "escritorio", "cuadro", "juguete", "alquiler", "mudanza", "casa", "pared", "chimenea", "comedor", "plaza", "calle", "estacionamiento", "parque", "puente", "puerto", "edificio", "escuela", "museo", "estatua", "fuente", "turista", "mendigo", "manejar", "acelerar", "frenar", "cruzar", "reparar", "conductor", "multa", "atasco", "carretera", "peaje", "curva", "florecer", "campo", "bosque", "huerto", "selva", "tronco", "rama", "flor", "hoja", "musgo", "cedro", "roble", "pino", "nogal", "naranjo", "tallo", "clavel", "margarita", "amapola", "rosa", "girasol", "violeta", "gato", "perro", "vaca", "pato", "oveja", "conejo", "pez", "oso", "jirafa", "erizo", "mariposa", "foca", "tigre", "cobra", "almeja", "paloma", "cisne", "mosquito", "hormiga", "llover", "nevar", "nublado", "soleado", "clima", "rayo", "nieve", "sol", "viento", "padre", "madre", "hijo", "abuela", "estudioso", "aula", "tiza", "regla", "computadora", "diccionario"]
+
+# creamos la funcion de obtener la palabra de
+
+
+def obtener_palabra_válida(palabras):
+    # seleccionar una palabra al azar de la lista
+    palabra = random.choice(palabras)
+
+    # Si la palabra contiene un guión o un espacio,
+    # seguir seleccionando una palabra al azar.
+    while '-' in palabra or ' ' in palabra:
+        palabra = random.choice(palabras)
+    # retorna la palabra en mayuscula
+    return palabra.upper()
+
+# se crea la funcion para el juego
+
+
+def ahorcado():
+    # se imprime mensaje de bienvenida
+    print("***********************************")
+    print("****** ¡Bienvenid@ al juego! ******")
+    print("***********************************")
+    # guardamos el return de obtener palabra en la variable
+    palabra = obtener_palabra_válida(palabras)
+    # obtenemos el conjunto de letras de la palabra que deben ser adivinadas.
+    letras_por_adivinar = set(palabra)
+    # obtenemos el conjunto de letras en el abecedario.
+    abecedario = set(string.ascii_uppercase)
+    # guardaremos las letras que el usuario ha advinado durante el juego.
+    letras_adivinadas = set()
+    # creamos variable para las vidas y lo intentos que tiene para lograr ganar el juego
+    vidas = 7
+
+    # Obtener respuesta del usuario mientras existan
+    # letras pendientes y al jugador le queden vidas.
+    while len(letras_por_adivinar) > 0 and vidas > 0:
+        # Mostramos en pantalla las Letras adivinadas:
+        print(
+            f"Te quedan {vidas} vidas y has usado estas letras: {' '.join(letras_adivinadas)}")
+
+        # Estado actual de la palabra que el jugador debe adivinar
+        palabra_lista = [
+            letra if letra in letras_adivinadas else '-' for letra in palabra]
+        # mostrar en pantalla el estado del ahorcado.
+        print(vidas_diccionario_visual[vidas])
+        # se mostrara las letras separadas por un espacio.
+        print(f"Palabra: {' '.join(palabra_lista)}")
+
+        # El usuario escoge una letra nueva y convertimos en mayuscula
+        letra_usuario = input('Escoge una letra: ').upper()
+
+        # Si la letra escogida por el usuario está en el abecedario
+        # y no está en el conjunto de letras que ya se han ingresado,
+        # se añade la letra al conjunto de letras ingresadas.
+        if letra_usuario in abecedario - letras_adivinadas:
+            letras_adivinadas.add(letra_usuario)
+            # Si la letra está en la palabra, quitar la letra
+            # del conjunto de letras pendientes por adivinar.
+            if letra_usuario in letras_por_adivinar:
+                letras_por_adivinar.remove(letra_usuario)
+                print('')
+            # Si la letra no está en la palabra, quitar una vida.
+            else:
+                vidas = vidas - 1
+                print(f"\nTu letra, {letra_usuario} no está en la palabra.")
+        # Si la letra escogida por el usuario ya fue ingresada.
+        elif letra_usuario in letras_adivinadas:
+            print("\nYa escogiste esa letra. Por favor escoge una letra nueva.")
+        else:
+            print("\nEsta letra no es válida.")
+
+    # El juego llega a esta línea cuando se agotan las vidas del jugador
+    # o cuando se adivinan todas las letras de la palabra.
+    if vidas == 0:
+        print(vidas_diccionario_visual[vidas])
+        print(
+            f"¡Ahorcado! Perdiste. Lo lamento mucho. La palabra era: {palabra}")
+    else:
+        print(f'¡Excelente! ¡Adivinaste la palabra {palabra}!')
+
+
+if __name__ == '__main__':
+    ahorcado()
